9ef19607791f247e1de17103c78f972ead8ed91f
Umair <mrumairhassan@outlook.com>
2022-08-26 00:47:47 +0500

    Again

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  let [counter, setCounter] = useState(1);
+  setCounter(2);
   return (
-    <div >
-      <h3>Hello</h3>
+    <div>
+      <h1>Hello{counter}</h1>
     </div>
-  );
-}
+  )}
 
 export default App;
