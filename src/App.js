93feb9ca9352ed9db14addf604d72f667da58e11
Umair <mrumairhassan@outlook.com>
2022-08-26 01:44:01 +0500

    Value pass through props value is update on message and show in app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import React, { useState } from "react"; 
+import {Message} from './Message.js'
 import "./App.css";
 
 function App() {
   let [count, setCount] = useState(0)
 
-    setCount(2)
+    //setCount(2)
   
   return (
     <div>
+      <Message counter={count}/>
       <h3>Hello the value of count is : {count}</h3>
       <button onClick={()=> setCount(++count)}>Update Counter</button>
     </div>
---
+++ src/Message.js
@@ -0,0 +1,13 @@
+import React from 'react';  
+
+export  function Message(props){
+
+
+
+
+    return(
+        <div>
+            <h1>The value of counter variable is {props.counter}</h1>
+        </div>
+    )
+}
